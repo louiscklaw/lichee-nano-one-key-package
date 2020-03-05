865e9538afae67b4e7050a0c02490e4730cc31cd
louiscklaw <louiscklaw@gmail.com>
2020-03-05 22:04:47 +0800

    update for docker image,

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+set -xe
+
+if [ -z "$DOCKER_ENVIRONEMNT" ]
+then
+  echo "ERROR: should be launch inside docker "
+  exit 1
+else
+  echo "checked docker environment, continue"
+fi
+
 cd /root
   cd scripts
     ./init.sh
