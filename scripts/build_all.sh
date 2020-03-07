1d5772b98e12a085b545f7de3378747ab154c9f4
louiscklaw <louiscklaw@gmail.com>
2020-03-07 20:56:17 +0800

    update build_all script,

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -11,17 +11,17 @@ else
 fi
 
 cd /root
-  # cd scripts
-  #   ./init.sh
-  # cd ..
+  cd scripts
+    ./init.sh
+  cd ..
 
-  # cd uboot_ws
-  #   ./build_uboot_spi.sh
-  # cd ..
+  cd uboot_ws
+    ./build_uboot_spi.sh
+  cd ..
 
-  # cd rootfs_ws
-  #   ./build_rootfs_spi.sh
-  # cd ..
+  cd rootfs_ws
+    ./build_rootfs_spi.sh
+  cd ..
 
   cd linux_ws
     ./build_linux_spi.sh
