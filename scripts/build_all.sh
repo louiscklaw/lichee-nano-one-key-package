dd4ccf83f8b48045bcdc791afa0eaeb3e9bf60d0
louiscklaw <louiscklaw@gmail.com>
2020-03-29 00:04:04 +0800

    add echo text,

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -43,11 +43,13 @@ cd /root
 
     if [ -n "$SEQUENTIAL_BUILD" ]
     then
+      echo "start sequential build"
       ./build_dts.sh | tee build_dts.log
       ./build_zImage.sh | tee build_zImage.log
       ./build_rootfs.sh | tee build_rootfs.log
       ./build_uboot.sh  | tee build_uboot.log
     else
+      echo "start parallel build"
       ./build_dts.sh | tee build_dts.log &
       ./build_zImage.sh | tee build_zImage.log &
       ./build_rootfs.sh | tee build_rootfs.log &
