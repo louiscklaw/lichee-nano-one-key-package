e40efac1e5084b1c349d601bff0122f84c7101ce
louiscklaw <louiscklaw@gmail.com>
2020-03-29 14:00:42 +0800

    update and test,

--- scripts/build_zImage.sh
+++ scripts/build_zImage.sh
@@ -11,8 +11,19 @@ else
 fi
 
 cd /root
+  cd scripts
+    if [ -z "$INSTALL_BUILD_TOOLS" ]
+    then
+      echo "skipping install tools"
+    else
+      ./init.sh > init.log
+    fi
+  cd ..
+cd ..
+
+cd /root
   cd linux_ws
-    ./build_linux_spi.sh
+    ./build_linux_spi.sh > build_linux_spi.log
   cd ..
 cd ..
 
