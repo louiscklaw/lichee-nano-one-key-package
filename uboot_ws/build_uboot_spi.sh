5e5b25dc4f0960e6aab182167b491fc40817eb79
louiscklaw <louiscklaw@gmail.com>
2020-03-29 13:57:03 +0800

    update and test,

--- uboot_ws/build_uboot_spi.sh
+++ uboot_ws/build_uboot_spi.sh
@@ -9,8 +9,8 @@ sudo rm -rf ../output/u-boot-sunxi-with-spl.bin
 
 cd u-boot
 
-  printf "\nclear old copy\n"
-  rm -rf u-boot-sunxi-with-spl.bin
+  printf "\ncheck if old copy exist\n"
+  # rm -rf u-boot-sunxi-with-spl.bin
   ls -lh u-boot-sunxi-with-spl.bin
 
   # cp ../.config_spi ./.config
@@ -19,7 +19,7 @@ cd u-boot
 
   # make ARCH=arm menuconfig
   # cp ./.config ../.config_spi
-  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j6
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j2
 
   # list output file
   printf "\nlist output file\n"
