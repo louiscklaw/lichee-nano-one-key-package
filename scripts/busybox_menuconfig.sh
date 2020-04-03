c8f05729371c4f00ad2a06b1c9a650a637c4dd9b
louiscklaw <louiscklaw@gmail.com>
2020-04-03 16:53:05 +0800

    update to enable timeout,

--- scripts/busybox_menuconfig.sh
+++ scripts/busybox_menuconfig.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cp rootfs_ws/.config/busybox.config rootfs_ws/buildroot-2019.11.1/package/busybox/busybox.config
+
+# /home/logic/_workspace/lichee-nano-one-key-package/rootfs_ws/buildroot-2019.11.1/output/build/busybox-1.31.1/.config
+cp rootfs_ws/.config/busybox.config rootfs_ws/buildroot-2019.11.1/output/build/busybox-1.31.1/.configbusybox.config
 
 cd rootfs_ws/buildroot-2019.11.1
   make ARCH=arm busybox-menuconfig
 cd -
 
-cp rootfs_ws/buildroot-2019.11.1/package/busybox/busybox.config rootfs_ws/.config/busybox.config
+cp rootfs_ws/buildroot-2019.11.1/output/build/busybox-1.31.1/.config rootfs_ws/.config/busybox.config
